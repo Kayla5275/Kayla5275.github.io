7a89deba72eb656d65addf6a1b8e5da7c6b23842
Kayla5275 <kaylag0505@gmail.com>
2025-05-15 23:35:56 -0600

    Starting Thropic website from scratch

---
+++ Thropic/script.js
@@ -0,0 +1,14 @@
+// script.js
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute('href'));
+      if (target) {
+        target.scrollIntoView({
+          behavior: 'smooth'
+        });
+      }
+    });
+  });
+  
